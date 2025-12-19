a452e91baa0a5db13e27fc777482e87b220dd714
PrakharTagra <prakhartagra16@gmail.com>
2025-12-19 16:33:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -210,4 +210,7 @@ app.post("/post/delete/:userid/:postid",async (req,res)=>{
     res.redirect(`/posts/${req.params.userid}`)
 })
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
